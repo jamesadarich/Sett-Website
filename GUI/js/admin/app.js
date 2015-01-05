9a5aeea6f7f9d20dfa13f5b6e46ab73f0e64acd4
jamesrichford <jamesrichford@googlemail.com>
2015-01-05 00:27:27 +0000

    Added API domain references to admin

--- GUI/js/admin/app.js
+++ GUI/js/admin/app.js
@@ -1,4 +1,8 @@
-﻿if (app === undefined) {
+﻿if (apiUrl === undefined) {
+    var apiUrl = 'http://api.getsett.net';
+}
+
+if (app === undefined) {
     var app = angular.module('sett-site-admin', ['ngMaterial']);
 }
 
--- GUI/js/app.js
+++ GUI/js/app.js
@@ -1,4 +1,6 @@
-﻿var apiUrl = 'http://api.getsett.net'
+﻿if (apiUrl === undefined) {
+    var apiUrl = 'http://api.getsett.net';
+}
 
 if (app === undefined) {
     var app = angular.module('sett-site', ['ngMaterial']);
