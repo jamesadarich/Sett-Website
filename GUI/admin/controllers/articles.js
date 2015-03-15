0c35c3a219327e4bbb28901f0c06ab2ca88d3caf
jamesrichford <jamesrichford@googlemail.com>
2015-03-15 23:00:17 +0000

    Lots of new lovely stuff

---
+++ GUI/admin/controllers/articles.js
@@ -0,0 +1,14 @@
+﻿app.controller("article-controller", function ($scope) {
+    $scope.articles = [];
+
+    $.get(apiUrl + '/articles')
+    .success(function (articles) {
+        $scope.articles = articles;
+        $scope.$apply();
+    });
+
+    $scope.newArticle = function () {
+
+        window.location.replace('/admin/edit-article.html');
+    }
+});
