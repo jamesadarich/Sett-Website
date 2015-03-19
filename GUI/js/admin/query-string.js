a67730a460c5b595d90d3b5703083d180ebc4e36
jamesrichford <jamesrichford@googlemail.com>
2015-03-19 20:15:09 +0000

    Slimming down!

--- GUI/js/admin/query-string.js
+++
@@ -1,8 +0,0 @@
-﻿var queryString = {};
-
-queryString.parameter = function(name) {
-    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-        results = regex.exec(location.search);
-    return results === null ? null : decodeURIComponent(results[1].replace(/\+/g, " "));
-}
--- Sett Website/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Sett_Website.WebApiApplication" Language="C#" %>
