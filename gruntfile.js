7d778a345e2d5b9a0cf6988c17053fb17ceb3185
jamesrichford <jamesrichford@googlemail.com>
2015-04-01 23:05:47 +0100

    Ruby-less Sass

--- gruntfile.js
+++ gruntfile.js
@@ -13,21 +13,22 @@ module.exports = function(grunt) {
       }
     },
     sass: {
-      dist: {
-        files: [{
-          expand: true,
-          cwd: 'sass',
-          src: ['*.scss'],
-          dest: 'css',
-          ext: '.css'
-        }]
-      }
-    }
+			options: {
+				sourceMap: true,
+				outputStyle: 'compressed'
+			},
+			dist: {
+				files: {
+					'css/global.css': 'sass/global.scss',
+          'css/article.css': 'sass/article.scss'
+				}
+			}
+		}
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-sass');
 
   // Default task(s).
   grunt.registerTask('default', ['uglify']);
