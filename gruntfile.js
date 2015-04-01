3946249aa022d5f2b9e96e5a6b06cecc3ccc6103
jamesrichford <jamesrichford@googlemail.com>
2015-04-01 19:06:50 +0100

    Migrate from Visual Studio to Atom
    Migrating from Visual Studio to Atom and tidying up all the extra
    gubbins that goes along with it.

---
+++ gruntfile.js
@@ -0,0 +1,36 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'js/*.js',
+        dest: 'build/<%= pkg.name %>.min.js'
+      }
+    },
+    sass: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: 'sass',
+          src: ['*.scss'],
+          dest: 'compiled-css',
+          ext: '.css'
+        }]
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+
+  // Default task(s).
+  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['sass']);
+
+};
