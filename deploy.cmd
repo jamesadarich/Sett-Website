17ff259718b718a701523a2ba48511d7120c1cdc
jamesrichford <jamesrichford@googlemail.com>
2015-04-01 23:14:44 +0100

    Move to deployment

--- deploy.cmd
+++ deploy.cmd
@@ -99,7 +99,10 @@ $NPM_CMD install bower
 $NPM_CMD install grunt-cli
 ./node_modules/.bin/grunt --no-color deploy
 
-"$KUDU_SYNC_CMD" -v 500 -f "$DEPLOYMENT_SOURCE/dist" -t "$DEPLOYMENT_TARGET" -n "$NEXT_MANIFEST_PATH" -p "$PREVIOUS_MANIFEST_PATH" -i ".git;.hg;.deployment;deploy.sh"
+IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
+  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
+    IF !ERRORLEVEL! NEQ 0 goto error
+)
 
 :: 2. Select node version
 ::call :SelectNodeVersion
