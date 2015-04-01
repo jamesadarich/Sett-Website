995cfd552fe7ed6801e596a6ec2c0c2a235eca76
jamesrichford <jamesrichford@googlemail.com>
2015-04-01 22:01:30 +0100

    Reattempt Grunt Deploy

--- deploy.cmd
+++ deploy.cmd
@@ -88,35 +88,44 @@ goto :EOF
 :Deployment
 echo Handling node.js deployment.
 
+$NPM_CMD install
 
-:: 2. Select node version
-call :SelectNodeVersion
-
-:: 3. Install npm packages
-IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
-  pushd "%DEPLOYMENT_TARGET%"
-  call :ExecuteCmd !NPM_CMD! install
-  IF !ERRORLEVEL! NEQ 0 goto error
-  popd
-)
-
-:: 5. Install Bower
 $NPM_CMD install bower
 ./node_modules/.bin/bower install
 
+$NPM_CMD install grunt-cli
+./node_modules/.bin/grunt --no-color deploy
+
+"$KUDU_SYNC_CMD" -v 500 -f "$DEPLOYMENT_SOURCE/dist" -t "$DEPLOYMENT_TARGET" -n "$NEXT_MANIFEST_PATH" -p "$PREVIOUS_MANIFEST_PATH" -i ".git;.hg;.deployment;deploy.sh"
+
+:: 2. Select node version
+::call :SelectNodeVersion
+
+:: 3. Install npm packages
+::IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
+  ::pushd "%DEPLOYMENT_TARGET%"
+  ::call :ExecuteCmd !NPM_CMD! install
+  ::  IF !ERRORLEVEL! NEQ 0 goto error
+  ::  popd
+::)
+
+:: 5. Install Bower
+::$NPM_CMD install bower
+::./node_modules/.bin/bower install
+
 :: 4. Run Grunt
 :: 4.1 Install Grunt locally
-call !NPM_CMD! install grunt-cli
+::call !NPM_CMD! install grunt-cli
 
 :: 4.2 Get Grunt to compile Sass
-call "!NODE_EXE!" ./node_modules/grunt-cli/bin/grunt --no-color deploy
+::call "!NODE_EXE!" ./node_modules/grunt-cli/bin/grunt --no-color deploy
 echo Sass compiled
 
 :: 1. KuduSync
-IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
-  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
-  IF !ERRORLEVEL! NEQ 0 goto error
-)
+::IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
+  ::  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
+  ::  IF !ERRORLEVEL! NEQ 0 goto error
+::)
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
